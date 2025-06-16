485c5347af1c146afeb1464deff43369639223cc
Kevin J. Sung <kevjsung@umich.edu>
2025-06-15 22:14:25 -0400

    add LUCJ CCSD state vector task

---
+++ scripts/run/lucj_ccsd_state_vector/n2_sto-6g_10e8o.py
@@ -0,0 +1,75 @@
+from __future__ import annotations
+
+import itertools
+import logging
+import os
+from concurrent.futures import ProcessPoolExecutor
+from pathlib import Path
+
+from tqdm import tqdm
+
+from lucj.params import LUCJParams
+from lucj.tasks.lucj_ccsd_state_vector_task import (
+    LUCJCCSDStateVectorTask,
+    run_lucj_ccsd_state_vector_task,
+)
+
+filename = f"logs/{os.path.splitext(os.path.relpath(__file__))[0]}.log"
+os.makedirs(os.path.dirname(filename), exist_ok=True)
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S %z",
+    filename=filename,
+)
+
+DATA_ROOT = Path(os.environ.get("LUCJ_DATA_ROOT", "data"))
+DATA_DIR = DATA_ROOT / os.path.basename(os.path.dirname(os.path.abspath(__file__)))
+MOLECULES_CATALOG_DIR = Path(os.environ.get("MOLECULES_CATALOG_DIR"))
+MAX_PROCESSES = 1
+OVERWRITE = False
+
+molecule_name = "n2"
+basis = "sto-6g"
+nelectron, norb = 10, 8
+molecule_basename = f"{molecule_name}_{basis}_{nelectron}e{norb}o"
+
+bond_distance_range = [0.9, 1.2, 1.5, 1.8]
+
+connectivities = ["square", "all-to-all"]
+n_reps_range = [1, 2, 3, None]
+
+tasks = [
+    LUCJCCSDStateVectorTask(
+        molecule_basename=molecule_basename,
+        bond_distance=d,
+        lucj_params=LUCJParams(
+            connectivity=connectivity,
+            n_reps=n_reps,
+            with_final_orbital_rotation=True,
+        ),
+    )
+    for connectivity, n_reps in itertools.product(connectivities, n_reps_range)
+    for d in bond_distance_range
+]
+
+if MAX_PROCESSES == 1:
+    for task in tqdm(tasks):
+        run_lucj_ccsd_state_vector_task(
+            task,
+            data_dir=DATA_DIR,
+            molecules_catalog_dir=MOLECULES_CATALOG_DIR,
+            overwrite=OVERWRITE,
+        )
+else:
+    with tqdm(total=len(tasks)) as progress:
+        with ProcessPoolExecutor(MAX_PROCESSES) as executor:
+            for task in tasks:
+                future = executor.submit(
+                    run_lucj_ccsd_state_vector_task,
+                    task,
+                    data_dir=DATA_DIR,
+                    molecules_catalog_dir=MOLECULES_CATALOG_DIR,
+                    overwrite=OVERWRITE,
+                )
+                future.add_done_callback(lambda _: progress.update())
