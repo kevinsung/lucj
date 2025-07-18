397e6a16bf8911ad2caa5b7c453feec799994775
WanHsuan.Lin <WanHsuan.Lin@ibm.com>
2025-07-18 10:57:24 -0400

    revise script for quimb

--- scripts/run/lucj_sqd_quimb/n2_6-31g_10e16o.py
+++ scripts/run/lucj_sqd_quimb/n2_6-31g_10e16o.py
@@ -25,7 +25,7 @@ logging.basicConfig(
 DATA_ROOT = Path(os.environ.get("LUCJ_DATA_ROOT", "data"))
 DATA_DIR = DATA_ROOT / os.path.basename(os.path.dirname(os.path.abspath(__file__)))
 MOLECULES_CATALOG_DIR = Path(os.environ.get("MOLECULES_CATALOG_DIR"))
-MAX_PROCESSES = 4
+MAX_PROCESSES = 16
 OVERWRITE = False
 
 molecule_name = "n2"
@@ -33,8 +33,8 @@ basis = "6-31g"
 nelectron, norb = 10, 16
 molecule_basename = f"{molecule_name}_{basis}_{nelectron}e{norb}o"
 
-bond_distance_range = [1.2]
-n_reps_range = [None, 1, 2, 3, 4, 6]
+bond_distance_range = [1.2, 2.4]
+n_reps_range = [1, 2, 3, 4, 6]
 connectivities = [
     # "square",
     # "all-to-all",
@@ -69,7 +69,7 @@ cutoffs = [
 seed = 0
 perm_mps = False
 max_dim_range = [250, 500]
-max_dim_range = [250]
+max_dim_range = [500]
 # TODO set limit on subspace dimension
 
 tasks = [
