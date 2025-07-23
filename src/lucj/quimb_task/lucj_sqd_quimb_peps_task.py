f3d8441075eed838c25ffb5820ba28482b23ab5b
WanHsuan.Lin <WanHsuanLin@ibm.com>
2025-07-23 17:42:23 -0400

    add peps

--- src/lucj/quimb_task/lucj_sqd_quimb_peps_task.py
+++ src/lucj/quimb_task/lucj_sqd_quimb_peps_task.py
@@ -18,7 +18,9 @@ from ffsim.variational.util import (
 from molecules_catalog.util import load_molecular_data
 from qiskit.primitives import BitArray
 from qiskit_addon_sqd.fermion import diagonalize_fermionic_hamiltonian, solve_sci_batch
-from lucj.tasks.lucj_compressed_t2_task_ffsim.compressed_t2 import from_t_amplitudes_compressed
+from lucj.tasks.lucj_compressed_t2_task_ffsim.compressed_t2 import (
+    from_t_amplitudes_compressed,
+)
 from lucj.params import COBYQAParams, LUCJParams, CompressedT2Params
 
 from qiskit.circuit import QuantumCircuit, QuantumRegister
@@ -40,8 +42,8 @@ class LUCJSQDQuimbPEPSTask:
     compressed_t2_params: CompressedT2Params | None
     connectivity_opt: bool = False
     random_op: bool = False
-    regularization: bool = False,
-    regularization_option: int = 0,
+    regularization: bool = (False,)
+    regularization_option: int = (0,)
     shots: int
     samples_per_batch: int
     n_batches: int
@@ -66,7 +68,9 @@ class LUCJSQDQuimbPEPSTask:
         elif self.compressed_t2_params is not None:
             compress_option = self.compressed_t2_params.dirpath
             if self.regularization:
-                compress_option = f"{compress_option}/regularization_{self.regularization_option}"
+                compress_option = (
+                    f"{compress_option}/regularization_{self.regularization_option}"
+                )
         else:
             compress_option = "truncated"
         return (
@@ -94,7 +98,7 @@ class LUCJSQDQuimbPEPSTask:
             / f"perm_mps-{self.perm_mps}"
             / f"seed-{self.seed}"
         )
-    
+
     @property
     def operatorpath(self) -> Path:
         if self.random_op:
@@ -104,7 +108,9 @@ class LUCJSQDQuimbPEPSTask:
         elif self.compressed_t2_params is not None:
             compress_option = self.compressed_t2_params.dirpath
             if self.regularization:
-                compress_option = f"{compress_option}/regularization_{self.regularization_option}"
+                compress_option = (
+                    f"{compress_option}/regularization_{self.regularization_option}"
+                )
         else:
             compress_option = "truncated"
         return (
@@ -130,7 +136,7 @@ def load_operator(task: LUCJSQDQuimbPEPSTask, data_dir: str, mol_data):
             norb,
             n_reps=task.lucj_params.n_reps,
             interaction_pairs=(pairs_aa, pairs_ab),
-            with_final_orbital_rotation=True
+            with_final_orbital_rotation=True,
         )
     elif task.connectivity_opt or task.compressed_t2_params is not None:
         operator_filename = data_dir / task.operatorpath / "operator.npz"
@@ -145,7 +151,9 @@ def load_operator(task: LUCJSQDQuimbPEPSTask, data_dir: str, mol_data):
         final_orbital_rotation = None
         if mol_data.ccsd_t1 is not None:
             final_orbital_rotation = (
-                ffsim.variational.util.orbital_rotation_from_t1_amplitudes(mol_data.ccsd_t1)
+                ffsim.variational.util.orbital_rotation_from_t1_amplitudes(
+                    mol_data.ccsd_t1
+                )
             )
 
         operator = ffsim.UCJOpSpinBalanced(
@@ -174,15 +182,18 @@ def load_operator(task: LUCJSQDQuimbPEPSTask, data_dir: str, mol_data):
                 interaction_pairs=interaction_pairs_spin_balanced(
                     connectivity=task.lucj_params.connectivity, norb=norb
                 ),
-            ) 
+            )
         else:
             operator = ffsim.UCJOpSpinBalanced.from_t_amplitudes(
                 mol_data.ccsd_t2,
                 n_reps=task.lucj_params.n_reps,
-                t1=mol_data.ccsd_t1 if task.lucj_params.with_final_orbital_rotation else None,
+                t1=mol_data.ccsd_t1
+                if task.lucj_params.with_final_orbital_rotation
+                else None,
                 interaction_pairs=(pairs_aa, pairs_ab),
             )
     return operator
+
 
 def run_lucj_sqd_quimb_task(
     task: LUCJSQDQuimbPEPSTask,
@@ -205,9 +216,7 @@ def run_lucj_sqd_quimb_task(
     ):
         logger.info(f"Data for {task} already exists. Skipping...\n")
         return task
-    intermediate_result_filename = (
-        data_dir / task.dirpath / "intermediate_data.pickle"
-    )
+    intermediate_result_filename = data_dir / task.dirpath / "intermediate_data.pickle"
 
     # Get molecular data and molecular Hamiltonian
     molecule_basename = task.molecule_basename
@@ -245,17 +254,20 @@ def run_lucj_sqd_quimb_task(
         decomposed = circuit.decompose(reps=2)
 
         # vacuum state
-        peps = quimb.tensor.TN_from_sites_product_state({q: [1.0, 0.0] for q in circuit.num_qubits})
+        peps = quimb.tensor.TN_from_sites_product_state(
+            {q: [1.0, 0.0] for q in range(circuit.num_qubits)}
+        )  # type: TensorNetworkGenVector
 
         # create size 1 bonds
         for i, j in pairs_aa:
             peps[i].new_bond(peps[j])
+            peps[i + norb].new_bond(peps[j + norb])
         for i, j in pairs_ab:
             peps[i].new_bond(peps[j + norb])
-        
+
         gauges = {}
 
-        for gate in tqdm.tqdm(quimb_gates(decomposed)):
+        for gate in quimb_gates(decomposed):
             peps.gate_simple_(
                 gate.array,
                 gate.qubits,
@@ -268,19 +280,28 @@ def run_lucj_sqd_quimb_task(
             )
 
         # the state normalization gives us an idea of fidelity
-        logger.info(f"{task}\n\tPeps fidelity {peps.normalize_simple(gauges)}") 
+        logger.info(f"{task}\n\tPeps fidelity {peps.normalize_simple(gauges)}")
 
         # equilibrate gauges
         peps.gauge_all_simple_(100, tol=1e-9, gauges=gauges, progbar=True)
 
         logger.info(f"{task}\n\tSampling circuit...")
         t0 = timeit.default_timer()
-        samples = peps.sample(task.shots, seed=task.seed)
+        samples = []
+        for _ in range(task.shots):
+            config, omega = peps.sample_configuration_cluster(
+                gauges=gauges,
+                seed=task.seed,
+                # single site clusters
+                max_distance=0,
+            )
+            x = "".join(map(str, (config[i] for i in range(circuit.num_qubits))))
+            samples.append(x)
         t1 = timeit.default_timer()
         time = t1 - t0
         logger.info(f"{task}\n\tDone sampling circuit in {time} seconds.")
         samples = [sample[::-1] for sample in samples]
-        
+
         bit_array = BitArray.from_samples(samples, num_bits=2 * norb)
         sci_solver = partial(solve_sci_batch, spin_sq=0.0)
         result = diagonalize_fermionic_hamiltonian(
@@ -298,7 +319,7 @@ def run_lucj_sqd_quimb_task(
             symmetrize_spin=task.symmetrize_spin,
             carryover_threshold=task.carryover_threshold,
             seed=rng,
-            max_dim=task.max_dim
+            max_dim=task.max_dim,
         )
         return result.energy + mol_data.core_energy
 
@@ -310,7 +331,9 @@ def run_lucj_sqd_quimb_task(
             operator, _, _ = from_t_amplitudes_compressed(
                 mol_data.ccsd_t2,
                 n_reps=task.lucj_params.n_reps,
-                t1=mol_data.ccsd_t1 if task.lucj_params.with_final_orbital_rotation else None,
+                t1=mol_data.ccsd_t1
+                if task.lucj_params.with_final_orbital_rotation
+                else None,
                 interaction_pairs=(pairs_aa, pairs_ab),
                 optimize=True,
             )
@@ -353,9 +376,12 @@ def run_lucj_sqd_quimb_task(
         with open(intermediate_result_filename, "wb") as f:
             pickle.dump(intermediate_result, f)
         if info["nit"] > 3:
-            if (abs(info["fun"][-1] - info["fun"][-2]) < 1e-5) and (abs(info["fun"][-2] - info["fun"][-3]) < 1e-5):
-                raise StopIteration("Objective function value does not decrease for two iterations.")
-
+            if (abs(info["fun"][-1] - info["fun"][-2]) < 1e-5) and (
+                abs(info["fun"][-2] - info["fun"][-3]) < 1e-5
+            ):
+                raise StopIteration(
+                    "Objective function value does not decrease for two iterations."
+                )
 
     t0 = timeit.default_timer()
     result = scipy.optimize.minimize(
