c57844138a8977b428021bddc80a39c48067f7ad
WanHsuan.Lin <WanHsuan.Lin@ibm.com>
2025-07-30 17:38:12 -0400

    add script for paper plot

--- scripts/run/lucj_sqd_quimb/n2_6-31g_10e16o.py
+++ scripts/run/lucj_sqd_quimb/n2_6-31g_10e16o.py
@@ -25,7 +25,7 @@ logging.basicConfig(
 DATA_ROOT = Path(os.environ.get("LUCJ_DATA_ROOT", "data"))
 DATA_DIR = DATA_ROOT / os.path.basename(os.path.dirname(os.path.abspath(__file__)))
 MOLECULES_CATALOG_DIR = Path(os.environ.get("MOLECULES_CATALOG_DIR"))
-MAX_PROCESSES = 1
+MAX_PROCESSES = 4
 OVERWRITE = False
 
 molecule_name = "n2"
