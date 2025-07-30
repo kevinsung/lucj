85e438e3ca2f75dda35e6a0fe0ec210732fc09d0
WanHsuan.Lin <WanHsuan.Lin@ibm.com>
2025-07-30 13:18:00 -0400

    upd plots

--- scripts/hardware/fe2s2_30e20o/lucj_truncated_t2.py
+++ scripts/hardware/fe2s2_30e20o/lucj_truncated_t2.py
@@ -38,15 +38,15 @@ n_reps_range = [1]
 
 shots = 100_000
 n_batches = 3
-energy_tol = 1e-5
-occupancies_tol = 1e-3
-carryover_threshold = 1e-3
-max_iterations = 100
+energy_tol = 1e-8
+occupancies_tol = 1e-5
+carryover_threshold = 1e-4
+max_iterations = 10
 symmetrize_spin = True
 # TODO set entropy and generate seeds properly
 entropy = 0
 
-max_dim = 4000
+max_dim = 2500
 samples_per_batch = max_dim
 
 tasks = [
