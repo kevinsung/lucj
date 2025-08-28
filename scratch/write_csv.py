f3c2b7665fa1ea4e1addca148bd3c5342d6090fd
WanHsuanLin <wanhsuanlin@g.ucla.edu>
2025-08-28 13:35:17 -0400

    add script for test hardware

---
+++ scratch/write_csv.py
@@ -0,0 +1,3 @@
+# csv 
+# columns: truncate, uccsd, uccsd compressed, compressed, compressed-stg, compressed-reg
+# row: opearator loss, vqe energy, sqd energy, sci subspace, hardware sqd, hardware sci subspace, hardware valid bitstring, hardware-fg valid bitstring
