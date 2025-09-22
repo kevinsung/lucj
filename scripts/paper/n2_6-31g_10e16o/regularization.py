62a421a26c8ea36015be7383e856aeb85b4e3039
Kevin J. Sung <kevjsung@umich.edu>
2025-09-22 15:14:37 +0000

    add regularization plot

---
+++ scripts/paper/n2_6-31g_10e16o/regularization.py
@@ -0,0 +1,344 @@
+import itertools
+import json
+import os
+import pickle
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+
+from lucj.params import CompressedT2Params, LUCJParams
+from lucj.sqd_energy_task.lucj_compressed_t2_task import SQDEnergyTask
+
+DATA_ROOT = Path(os.environ.get("LUCJ_DATA_ROOT", "data"))
+MOLECULES_CATALOG_DIR = Path(os.environ.get("MOLECULES_CATALOG_DIR"))
+
+molecule_name = "n2"
+basis = "6-31g"
+nelectron, norb = 10, 16
+molecule_basename = f"{molecule_name}_{basis}_{nelectron}e{norb}o"
+
+plots_dir = os.path.join("paper", molecule_basename)
+os.makedirs(plots_dir, exist_ok=True)
+
+bond_distances = [1.2, 2.4]
+
+connectivity = "all-to-all"
+
+n_reps_range = list(range(1, 11))
+
+shots = 100_000
+n_batches = 10
+energy_tol = 1e-5
+occupancies_tol = 1e-3
+carryover_threshold = 1e-3
+max_iterations = 1
+symmetrize_spin = True
+# TODO set entropy and generate seeds properly
+entropy = 0
+
+max_dim = 4000
+samples_per_batch = max_dim
+
+
+def load_data(filepath):
+    if not os.path.exists(filepath):
+        result = {
+            "energy": 0,
+            "error": 0,
+            "spin_squared": 0,
+            "sci_vec_shape": (0, 0),
+            "n_reps": 0,
+        }
+    else:
+        with open(filepath, "rb") as f:
+            result = pickle.load(f)
+    return result
+
+
+print("Done loading data.")
+
+linestyles = ["-.", ":", "--"]
+markers = ["o", "s", "v", "D", "p", "*", "P", "X"]
+
+with open("scripts/paper/color.json", "r") as file:
+    colors = json.load(file)
+
+
+fig, axes = plt.subplots(3, 2, figsize=(12, 10))
+
+
+for i, bond_distance in enumerate(bond_distances):
+    task_lucj_full = SQDEnergyTask(
+        molecule_basename=molecule_basename,
+        bond_distance=bond_distance,
+        lucj_params=LUCJParams(
+            connectivity=connectivity,
+            n_reps=None,
+            with_final_orbital_rotation=True,
+        ),
+        compressed_t2_params=None,
+        regularization=False,
+        regularization_option=None,
+        shots=shots,
+        samples_per_batch=samples_per_batch,
+        n_batches=n_batches,
+        energy_tol=energy_tol,
+        occupancies_tol=occupancies_tol,
+        carryover_threshold=carryover_threshold,
+        max_iterations=max_iterations,
+        symmetrize_spin=symmetrize_spin,
+        entropy=entropy,
+        max_dim=max_dim,
+    )
+    filepath = DATA_ROOT / task_lucj_full.operatorpath / "data.pickle"
+    results = load_data(filepath)
+    vqe_error_lucj_full = results["error"]
+    filepath = DATA_ROOT / task_lucj_full.dirpath / "sqd_data.pickle"
+    results = load_data(filepath)
+    sqd_error_lucj_full = results["error"]
+
+    tasks_compressed_t2 = [
+        SQDEnergyTask(
+            molecule_basename=molecule_basename,
+            bond_distance=bond_distance,
+            lucj_params=LUCJParams(
+                connectivity=connectivity,
+                n_reps=n_reps,
+                with_final_orbital_rotation=True,
+            ),
+            compressed_t2_params=CompressedT2Params(
+                multi_stage_optimization=True, begin_reps=20, step=2
+            ),
+            regularization=False,
+            regularization_option=None,
+            shots=shots,
+            samples_per_batch=samples_per_batch,
+            n_batches=n_batches,
+            energy_tol=energy_tol,
+            occupancies_tol=occupancies_tol,
+            carryover_threshold=carryover_threshold,
+            max_iterations=max_iterations,
+            symmetrize_spin=symmetrize_spin,
+            entropy=entropy,
+            max_dim=max_dim,
+        )
+        for n_reps in n_reps_range
+    ]
+
+    tasks_compressed_t2_naive = [
+        SQDEnergyTask(
+            molecule_basename=molecule_basename,
+            bond_distance=bond_distance,
+            lucj_params=LUCJParams(
+                connectivity=connectivity,
+                n_reps=n_reps,
+                with_final_orbital_rotation=True,
+            ),
+            compressed_t2_params=CompressedT2Params(
+                multi_stage_optimization=False, begin_reps=n_reps, step=2
+            ),
+            regularization=False,
+            regularization_option=None,
+            shots=shots,
+            samples_per_batch=samples_per_batch,
+            n_batches=n_batches,
+            energy_tol=energy_tol,
+            occupancies_tol=occupancies_tol,
+            carryover_threshold=carryover_threshold,
+            max_iterations=max_iterations,
+            symmetrize_spin=symmetrize_spin,
+            entropy=entropy,
+            max_dim=max_dim,
+        )
+        for n_reps in n_reps_range
+    ]
+
+    tasks_compressed_t2_reg = [
+        SQDEnergyTask(
+            molecule_basename=molecule_basename,
+            bond_distance=bond_distance,
+            lucj_params=LUCJParams(
+                connectivity=connectivity,
+                n_reps=n_reps,
+                with_final_orbital_rotation=True,
+            ),
+            compressed_t2_params=CompressedT2Params(
+                multi_stage_optimization=True, begin_reps=20, step=2
+            ),
+            regularization=True,
+            regularization_option=1,
+            regularization_factor=5e-3,
+            shots=shots,
+            samples_per_batch=samples_per_batch,
+            n_batches=n_batches,
+            energy_tol=energy_tol,
+            occupancies_tol=occupancies_tol,
+            carryover_threshold=carryover_threshold,
+            max_iterations=max_iterations,
+            symmetrize_spin=symmetrize_spin,
+            entropy=entropy,
+            max_dim=max_dim,
+        )
+        for n_reps in n_reps_range
+    ]
+
+    tasks_truncated = [
+        SQDEnergyTask(
+            molecule_basename=molecule_basename,
+            bond_distance=bond_distance,
+            lucj_params=LUCJParams(
+                connectivity=connectivity,
+                n_reps=n_reps,
+                with_final_orbital_rotation=True,
+            ),
+            random_op=False,
+            compressed_t2_params=None,
+            connectivity_opt=False,
+            regularization=False,
+            regularization_option=None,
+            shots=shots,
+            samples_per_batch=samples_per_batch,
+            n_batches=n_batches,
+            energy_tol=energy_tol,
+            occupancies_tol=occupancies_tol,
+            carryover_threshold=carryover_threshold,
+            max_iterations=max_iterations,
+            symmetrize_spin=symmetrize_spin,
+            entropy=entropy,
+            max_dim=max_dim,
+        )
+        for n_reps in n_reps_range
+    ]
+
+    list_loss = [[], [], []]
+    for task in tasks_compressed_t2_naive:
+        filepath = DATA_ROOT / task.operatorpath / "opt_data.pickle"
+        results = load_data(filepath)
+        list_loss[0].append(results["init_loss"])
+    for task in tasks_compressed_t2:
+        filepath = DATA_ROOT / task.operatorpath / "opt_data.pickle"
+        results = load_data(filepath)
+        list_loss[1].append(results["final_loss"])
+    for task in tasks_compressed_t2_reg:
+        filepath = DATA_ROOT / task.operatorpath / "opt_data.pickle"
+        results = load_data(filepath)
+        list_loss[2].append(results["final_loss"])
+    color_keys = ["lucj_truncated", "lucj_compressed", "lucj_compressed_reg"]
+    labels = ["LUCJ-truncated", "LUCJ-compressed", "LUCJ-compressed-reg"]
+    for loss, color_key, label, marker in zip(list_loss, color_keys, labels, markers):
+        axes[0, i].plot(
+            n_reps_range,
+            loss,
+            f"{marker}--",
+            markersize=5,
+            label=label,
+            color=colors[color_key],
+        )
+    axes[0, i].set_ylabel("Loss function", fontsize=12)
+    axes[0, i].set_xlabel("Repetitions", fontsize=12)
+    axes[0, i].set_xticks(n_reps_range)
+    axes[0, i].set_yscale("log")
+    axes[0, i].set_ylim(1e-4, 10)
+    axes[0, i].set_title(f"bond distance {bond_distance} Å", fontsize=16)
+
+    list_tasks = [
+        tasks_truncated,
+        tasks_compressed_t2,
+        tasks_compressed_t2_reg,
+    ]
+    color_keys = ["lucj_truncated", "lucj_compressed", "lucj_compressed_reg"]
+    labels = ["LUCJ-truncated", "LUCJ-compressed", "LUCJ-compressed-reg"]
+    for tasks, color_key, label, marker in zip(list_tasks, color_keys, labels, markers):
+        results = {}
+        for task in tasks:
+            filepath = DATA_ROOT / task.operatorpath / "data.pickle"
+            results[task] = load_data(filepath)
+        errors = [results[task]["error"] for task in tasks]
+        axes[1, i].plot(
+            n_reps_range,
+            errors,
+            f"{marker}--",
+            markersize=5,
+            label=label,
+            color=colors[color_key],
+        )
+    axes[1, i].text(
+        0.5,
+        0.95,
+        "VQE",
+        transform=axes[1, i].transAxes,
+        fontsize=12,
+        ha="center",
+        va="top",
+    )
+    axes[1, i].set_yscale("log")
+    axes[1, i].set_ylim(1e-2, 10)
+    axes[1, i].axhline(
+        vqe_error_lucj_full,
+        linestyle="--",
+        label="LUCJ-full",
+        color=colors["lucj_full"],
+    )
+    axes[1, i].set_ylabel("Energy error (Hartree)", fontsize=12)
+    axes[1, i].set_xlabel("Repetitions", fontsize=12)
+    axes[1, i].set_xticks(n_reps_range)
+
+    for tasks, color_key, label, marker in zip(list_tasks, color_keys, labels, markers):
+        results = {}
+        for task in tasks:
+            filepath = DATA_ROOT / task.dirpath / "sqd_data.pickle"
+            results[task] = load_data(filepath)
+        errors = [results[task]["error"] for task in tasks]
+        axes[2, i].plot(
+            n_reps_range,
+            errors,
+            f"{marker}--",
+            markersize=5,
+            label=label,
+            color=colors[color_key],
+        )
+    axes[2, i].text(
+        0.5,
+        0.95,
+        "QSCI",
+        transform=axes[2, i].transAxes,
+        fontsize=12,
+        ha="center",
+        va="top",
+    )
+    axes[2, i].set_yscale("log")
+    axes[2, i].set_ylim(1e-3, 1)
+    axes[2, i].axhline(
+        sqd_error_lucj_full,
+        linestyle="--",
+        label="LUCJ-full",
+        color=colors["lucj_full"],
+    )
+    axes[2, i].set_ylabel("Energy error (Hartree)", fontsize=12)
+    axes[2, i].set_xlabel("Repetitions", fontsize=12)
+    axes[2, i].set_xticks(n_reps_range)
+
+
+fig.suptitle(
+    f"N$_2$ 6-31G ({nelectron}e, {norb}o) all-to-all (UCJ)", fontsize=18
+)
+
+# axes[2, 0].legend()
+
+leg = axes[2, 0].legend(
+    bbox_to_anchor=(1, -0.25),
+    loc="upper center",
+    ncol=4,
+    columnspacing=1,
+    handletextpad=0.8,
+)
+leg.set_in_layout(False)
+plt.tight_layout()
+plt.subplots_adjust(top=0.91, bottom=0.1)
+
+filepath = os.path.join(
+    plots_dir,
+    f"{os.path.splitext(os.path.basename(__file__))[0]}.pdf",
+)
+plt.savefig(filepath)
+plt.close()
