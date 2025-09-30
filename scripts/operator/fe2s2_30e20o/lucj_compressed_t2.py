fb9ac3cef51e9d748ae159a862d106973f54674b
Kevin J. Sung <kevjsung@umich.edu>
2025-09-30 13:45:17 +0000

    update script

--- scripts/operator/fe2s2_30e20o/lucj_compressed_t2.py
+++ scripts/operator/fe2s2_30e20o/lucj_compressed_t2.py
@@ -24,11 +24,11 @@ logging.basicConfig(
     filename=filename,
 )
 
-DATA_ROOT = "/media/storage/WanHsuan.Lin/"
+DATA_ROOT = Path(os.environ.get("LUCJ_DATA_ROOT", "data"))
 # DATA_DIR = DATA_ROOT / os.path.basename(os.path.dirname(os.path.abspath(__file__)))
 DATA_DIR = DATA_ROOT 
 MOLECULES_CATALOG_DIR = Path(os.environ.get("MOLECULES_CATALOG_DIR"))
-MAX_PROCESSES = 4
+MAX_PROCESSES = 1
 OVERWRITE = False
 
 molecule_name = "fe2s2"
@@ -40,7 +40,7 @@ connectivities = [
     # "square",
     "all-to-all",
 ]
-n_reps_range = list(range(2, 12, 2)) + [None, 1, 3, 5, 7]
+n_reps_range = list(range(2, 12, 2)) + [1, 3, 5, 7]
 
 tasks = [
     LUCJCompressedT2Task(
