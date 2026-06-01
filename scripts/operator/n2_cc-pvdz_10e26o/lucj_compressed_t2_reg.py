c6924d849f61815347d26c796565333d216f8eba
Kevin J. Sung <kevjsung@umich.edu>
2026-06-01 20:31:55 +0000

    add scripts

---
+++ scripts/operator/n2_cc-pvdz_10e26o/lucj_compressed_t2_reg.py
@@ -0,0 +1,101 @@
+# (C) Copyright IBM 2025.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+from __future__ import annotations
+
+import itertools
+import logging
+import os
+from concurrent.futures import ProcessPoolExecutor
+from pathlib import Path
+
+import numpy as np
+from tqdm import tqdm
+
+from lucj.operator_task.lucj_compressed_t2_task import (
+    LUCJCompressedT2Task,
+    run_lucj_compressed_t2_task,
+)
+from lucj.params import CompressedT2Params, LUCJParams
+
+filename = f"logs/{os.path.splitext(os.path.relpath(__file__))[0]}.log"
+os.makedirs(os.path.dirname(filename), exist_ok=True)
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S %z",
+    filename=filename,
+)
+
+DATA_ROOT = Path(os.environ.get("LUCJ_DATA_ROOT", "data"))
+# DATA_DIR = DATA_ROOT / os.path.basename(os.path.dirname(os.path.abspath(__file__)))
+DATA_DIR = DATA_ROOT
+MOLECULES_CATALOG_DIR = Path(os.environ.get("MOLECULES_CATALOG_DIR"))
+MAX_PROCESSES = 8
+OVERWRITE = True
+
+molecule_name = "n2"
+basis = "cc-pvdz"
+nelectron, norb = 10, 26
+molecule_basename = f"{molecule_name}_{basis}_{nelectron}e{norb}o"
+
+bond_distance_range = [1.2, 2.4]
+
+connectivities = [
+    "heavy-hex",
+    # "square",
+    # "all-to-all",
+]
+n_reps_range = list(range(1, 11, 1))
+regularization_options = [1]
+regularization_factors = [1e-1]
+
+tasks = [
+    LUCJCompressedT2Task(
+        molecule_basename=molecule_basename,
+        bond_distance=d,
+        lucj_params=LUCJParams(
+            connectivity=connectivity,
+            n_reps=n_reps,
+            with_final_orbital_rotation=True,
+        ),
+        compressed_t2_params=CompressedT2Params(
+            multi_stage_optimization=True, begin_reps=50, step=2
+        ),
+        regularization=True,
+        regularization_option=regularization_option,
+        regularization_factor=regularization_factor,
+    )
+    for regularization_option in regularization_options
+    for regularization_factor in regularization_factors
+    for d in bond_distance_range
+    for connectivity, n_reps in itertools.product(connectivities, n_reps_range)
+]
+
+if MAX_PROCESSES == 1:
+    for task in tqdm(tasks):
+        run_lucj_compressed_t2_task(
+            task,
+            data_dir=DATA_DIR,
+            molecules_catalog_dir=MOLECULES_CATALOG_DIR,
+            overwrite=OVERWRITE,
+        )
+else:
+    with tqdm(total=len(tasks)) as progress:
+        with ProcessPoolExecutor(MAX_PROCESSES) as executor:
+            for task in tasks:
+                future = executor.submit(
+                    run_lucj_compressed_t2_task,
+                    task,
+                    data_dir=DATA_DIR,
+                    molecules_catalog_dir=MOLECULES_CATALOG_DIR,
+                    overwrite=OVERWRITE,
+                )
+                future.add_done_callback(lambda _: progress.update())
---
+++ scripts/sqd/n2_cc-pvdz_10e26o/lucj_compressed_t2_reg.py
@@ -0,0 +1,121 @@
+# (C) Copyright IBM 2025.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+from __future__ import annotations
+import logging
+import os
+from concurrent.futures import ProcessPoolExecutor
+from pathlib import Path
+
+from tqdm import tqdm
+
+from lucj.params import LUCJParams, CompressedT2Params
+from lucj.sqd_energy_task.lucj_compressed_t2_task import (
+    SQDEnergyTask,
+    run_sqd_energy_task,
+)
+
+filename = f"logs/{os.path.splitext(os.path.relpath(__file__))[0]}.log"
+os.makedirs(os.path.dirname(filename), exist_ok=True)
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S %z",
+    filename=filename,
+)
+
+DATA_ROOT = Path(os.environ.get("LUCJ_DATA_ROOT", "data"))
+# DATA_DIR = DATA_ROOT / os.path.basename(os.path.dirname(os.path.abspath(__file__)))
+DATA_DIR = DATA_ROOT
+MOLECULES_CATALOG_DIR = Path(os.environ.get("MOLECULES_CATALOG_DIR"))
+MAX_PROCESSES = 1
+OVERWRITE = False
+
+molecule_name = "n2"
+basis = "cc-pvdz"
+nelectron, norb = 10, 26
+molecule_basename = f"{molecule_name}_{basis}_{nelectron}e{norb}o"
+
+bond_distance_range = [1.2, 2.4]
+
+connectivities = [
+    "heavy-hex",
+    # "square",
+    # "all-to-all",
+]
+n_reps_range = [1]
+shots = 1_000_000
+n_batches = 1
+energy_tol = 1e-5
+occupancies_tol = 1e-3
+carryover_threshold = 1e-3
+max_iterations = 1
+symmetrize_spin = True
+# TODO set entropy and generate seeds properly
+entropy = 0
+
+max_dim = None
+samples_per_batch = shots
+regularization_factors = [1e-1]
+
+tasks_reg1 = [
+    SQDEnergyTask(
+        molecule_basename=molecule_basename,
+        bond_distance=d,
+        lucj_params=LUCJParams(
+            connectivity=connectivity,
+            n_reps=n_reps,
+            with_final_orbital_rotation=True,
+        ),
+        compressed_t2_params=CompressedT2Params(
+            multi_stage_optimization=True, begin_reps=50, step=2
+        ),
+        regularization=True,
+        regularization_option=1,
+        regularization_factor=regularization_factor,
+        shots=shots,
+        samples_per_batch=samples_per_batch,
+        n_batches=n_batches,
+        energy_tol=energy_tol,
+        occupancies_tol=occupancies_tol,
+        carryover_threshold=carryover_threshold,
+        max_iterations=max_iterations,
+        symmetrize_spin=symmetrize_spin,
+        entropy=entropy,
+        max_dim=max_dim,
+    )
+    for n_reps in n_reps_range
+    for connectivity in connectivities
+    for d in bond_distance_range
+    for regularization_factor in regularization_factors
+]
+
+tasks = tasks_reg1  # tasks_reg2 + tasks_reg0 + tasks_reg1
+
+if MAX_PROCESSES == 1:
+    for task in tqdm(tasks):
+        run_sqd_energy_task(
+            task,
+            data_dir=DATA_DIR,
+            molecules_catalog_dir=MOLECULES_CATALOG_DIR,
+            overwrite=OVERWRITE,
+        )
+else:
+    with tqdm(total=len(tasks)) as progress:
+        with ProcessPoolExecutor(MAX_PROCESSES) as executor:
+            for task in tasks:
+                future = executor.submit(
+                    run_sqd_energy_task,
+                    task,
+                    data_dir=DATA_DIR,
+                    molecules_catalog_dir=MOLECULES_CATALOG_DIR,
+                    overwrite=OVERWRITE,
+                )
+                future.add_done_callback(lambda _: progress.update())
